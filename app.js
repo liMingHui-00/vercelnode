b171e803011038da64389aa49c4866ea2c05afbb
liminghui <3209498857@qq.com>
2024-05-05 23:50:50 +0800

    修改

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const express = require("express")
-const cors = require("cors")
 const bodyParser = require("body-parser")
-
+const mysql = require("mysql")
+const cors = require("cors")
 const app = express()
-const port = 3000 // 设置服务器端口号
-// 处理跨域
-app.use(cors({ origin: "*" }))
-app.use(bodyParser)
-// 静态资源中间件
-app.use(express.static(__dirname + "/public"))
-// 新闻接口
-const newsRouter = require("./router/news/index")
-// 用户接口
-const userRouter = require("./router/user/index")
-const oldersRouter = require("./router/orders")
-// olders接口
-app.use(oldersRouter)
-app.use(newsRouter)
-app.use(userRouter)
-// 启动服务器
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.use(bodyParser.json())
+// 生成salt
+const saltRounds = 10
+app.use("/public", express.static("public"))
+app.use(
+  cors({
+    origin: "*",
+  })
+)
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "news_box",
 })
+// 获取用户信息
+app.get("/getuserinfo", (req, res) => {
+  const query = "select * from user"
+  connection.query(query, (error, results) => {
+    if (error) {
+      res.status(500).send("获取用户信息失败", error)
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      })
+    }
+  })
+})
+app.listen(3000, () => console.log("Server started on port 3000"))
